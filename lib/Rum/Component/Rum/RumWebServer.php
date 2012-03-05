2aba04d182d119e6124eee8150518f6f080b45b7
Matthias Vandermaesen <matthias@colada.be>
2012-03-05 08:35:12 +0100

    Added: exception if the configured web server type is not implemented

--- lib/Rum/Component/Rum/RumWebServer.php
+++ lib/Rum/Component/Rum/RumWebServer.php
@@ -4,6 +4,7 @@ namespace Rum\Component\Rum;
 
 use Rum\Component\Rum\RumDecorator;
 use Rum\Component\WebServer\WebServer;
+use Rum\Component\WebServer\Exception\RumWebServerClassNotFound;
 
 class RumWebServer extends RumDecorator {
 
@@ -11,8 +12,22 @@ class RumWebServer extends RumDecorator {
 
   private $file_system;
 
+  const RUM_HTTP_APACHE = 'Apache';
+
+  const RUM_HTTP_NGINX = 'Nginx';
+
   function __construct($rum) {
     parent::__construct($rum);
+    $this->checkSetting('rum_http_type');
+    $class_name = drush_get_option('rum_http_type', '');
+    switch ($class_name) {
+      case self::RUM_HTTP_APACHE :
+      case self::RUM_HTTP_NGINX :
+        $this->web_server = WebServer::getInstance($class_name);
+        break;
+      default :
+        throw new RumWebServerClassNotFound($class_name);
+    }
     $this->web_server = WebServer::getInstance('Apache');
     $settings = array('rum_http_port');
     $settings += $this->web_server->getSettings();
