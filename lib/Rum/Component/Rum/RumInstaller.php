7ef0a3f6d64f0727380a725c7ecedad5d6f65f87
Matthias Vandermaesen <matthias@colada.be>
2012-04-06 22:46:46 +0200

    Improved: Rum now reads config options from cli and applies them during installation.

--- lib/Rum/Component/Rum/RumInstaller.php
+++ lib/Rum/Component/Rum/RumInstaller.php
@@ -20,15 +20,23 @@ class RumInstaller extends RumDecorator {
     if (!drush_bootstrap_max_to_sitealias($site_record, DRUSH_BOOTSTRAP_DRUPAL_CONFIGURATION)) {
       throw new RumBootstrapDrupalConfigurationFailed();
     }
-    // Make this configurable? Provide defaults!
-    $options['uid1_account'] = drush_prompt(dt('Enter the administrator (uid=1) account name'));
-    $options['uid1_pass'] = drush_prompt(dt('Enter the administrator (uid=1) password'));
-    $options['uid1_mail'] = drush_prompt(dt('Enter the administrator (uid=1) e-mail address'));
+
+    $options['account-name'] = drush_prompt(dt('Enter the administrator (uid=1) account name'));
+    $options['account-pass'] = drush_prompt(dt('Enter the administrator (uid=1) password'));
+    $options['account-mail'] = drush_prompt(dt('Enter the administrator (uid=1) e-mail address'));
     $options['locale'] = drush_prompt(dt('Enter your desired locale'));
-    $options['site_name'] = drush_prompt(dt('Enter the name of your site'));
-    $options['site_mail'] = drush_prompt(dt('Enter the global mail address of your site'));
+    $options['site-name'] = drush_prompt(dt('Enter the name of your site'));
+    $options['site-mail'] = drush_prompt(dt('Enter the global mail address of your site'));
+
+    // Setting the options as a drush command specific context so the site install 
+    // routine picks it up.
+    drush_set_context('specific', $options);
+
+     // Determin the major version and launch version specific installation.
     drush_include_engine('drupal', 'site_install', drush_drupal_major_version());
     drush_core_site_install_version($profile, $options);
+
+    drush_log(dt('Installation finished.'), 'success');
   }
 
 }
