89915a20981f1f326fe20b2c96c58f78282a7a96
Matthias Vandermaesen <matthias@colada.be>
2012-04-08 19:24:19 +0200

    Bootstrapping crush lives in its own method in Rum class now

--- lib/Rum/Component/Rum/RumInstaller.php
+++ lib/Rum/Component/Rum/RumInstaller.php
@@ -3,24 +3,15 @@
 namespace Rum\Component\Rum;
 
 use Rum\Component\Rum\RumDecorator;
-use Rum\Component\Rum\Exception\RumBootstrapDrupalConfigurationFailed;
+use Rum\Component\Rum\FileSystem;
 
 class RumInstaller extends RumDecorator {
-
-  private $rum;
 
   public function __construct($rum) {
     parent::__construct($rum);
   }
 
   public function install($profile) {
-    // The project name is the alias name of our site
-    $alias = '@' . $this->getProjectName();
-    $site_record = drush_sitealias_get_record($alias);
-    if (!drush_bootstrap_max_to_sitealias($site_record, DRUSH_BOOTSTRAP_DRUPAL_CONFIGURATION)) {
-      throw new RumBootstrapDrupalConfigurationFailed();
-    }
-
     $options['account-name'] = drush_prompt(dt('Enter the administrator (uid=1) account name'));
     $options['account-pass'] = drush_prompt(dt('Enter the administrator (uid=1) password'));
     $options['account-mail'] = drush_prompt(dt('Enter the administrator (uid=1) e-mail address'));
