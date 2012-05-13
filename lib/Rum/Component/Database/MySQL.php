dabdab9941955329c96e4dda49128c520b27674b
Matthias Vandermaesen <matthias@colada.be>
2012-05-13 17:48:31 +0200

    Added: documentation to the MySQL class

--- lib/Rum/Component/Database/MySQL.php
+++ lib/Rum/Component/Database/MySQL.php
@@ -1,9 +1,23 @@
 <?php
+/**
+ * @file
+ * Contains the MySQL driver for Rum
+ *
+ * The class in this file adds MySQL support to Rum through a concrete implementation
+ * of the Database class which can be instantiated.
+ */
 
 namespace Rum\Component\Database;
 
 use Rum\Component\Database\Database;
 
+/**
+ * MySQL driver class
+ *
+ * This class extends and implements the abstract Database class. It provides
+ * a concrete implementation to connect and perform actions against a MySQL
+ * database system.
+ */
 class MySQL extends Database {
 
   protected $db_root_user;
@@ -15,15 +29,24 @@ class MySQL extends Database {
   }
 
   /**
-   * Creates a new MySQl user
-   * 
+   * Creates a new MySQl user and database
+   *
+   * When creating a new database user, a corresponding database will be created
+   * and the user will be granted all privileges on this database. Each database
+   * user can only access its' corresponding database rather then using a general
+   * account which is granted privileges to every database. Before we create
+   * the user and database, the function drops any existing user and database
+   * before creating them anew.
+   *
    * @todo Figure out how to write this in a cleaner way. With error handling.
-   * 
-   * @param type $db_user
-   * @param type $db_cred 
+   *
+   * @param $db_user
+   *   The database user that will be created
+   * @param $db_cred
+   *   The password that will be used for this user. This should be a strong
+   *   password.
    */
   public function createUser($db_user, $db_cred) {
-
     // Create harmless privilige first so the user is created in case it does not exist.
     $harmless = $this->baseCommand() . "GRANT USAGE ON *.* TO '" . $db_user . "'@'localhost';\"";
     drush_shell_exec($harmless);
